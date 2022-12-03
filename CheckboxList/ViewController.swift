fb22a573b5aa3a73e050aef6356cde7d4e7f3c91
Mahesh Kumawat <mahesh.kumawat@metacube.com>
2022-12-03 11:37:56 +0530

    Initial Commit

---
+++ CheckboxList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckboxList
+//
+//  Created by Mahesh Kumawat on 03/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
